94b6bc922a2facfc170525cae41415fdf228c879
abhi <ajayanthi2@gmail.com>
2022-12-19 14:41:19 -0500

    updated comments

--- bike-shop.sh
+++ bike-shop.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#Create a PSQL Variable so you can query psql with $($PSQL "<query here>")
+#set a variable so that psql database can be queried with $($PSQL "query here")
 PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+
 echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
 
-
-#Build Main menu first, create dummy sub menus with echo statements to test flow
 MAIN_MENU() {
-#Checks if MAIN_MENU function call is followed by an argument - if it is, print the argument
+  #if MAIN_MENU is called with an argument ($1), echo the argument
   if [[ $1 ]]
   then
     echo -e "\n$1"
   fi
-#Begin menu here
+
   echo "How may I help you?" 
   echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
   read MAIN_MENU_SELECTION
@@ -26,11 +25,30 @@ MAIN_MENU() {
 
 RENT_MENU() {
   # get available bikes
+  AVAILABLE_BIKES=$($PSQL "SELECT bike_id, type, size FROM bikes WHERE available = true ORDER BY bike_id")
+  
 
   # if no bikes available
+  # -z checks if $AVAILABLE_BIKES is null
+  if [[ -z $AVAILABLE_BIKES ]]
+  then
+    # send to main menu
+    MAIN_MENU "Sorry, we don't have any bikes available right now."
+  else
+    # display available bikes
+    echo -e "\nHere are the bikes we have available:"
+    #bar refers to the | symbol in the psql output - this reads them into variables but then omits them when printing
+    echo "$AVAILABLE_BIKES" | while read BIKE_ID BAR TYPE BAR SIZE
+      do
+        echo "$BIKE_ID $TYPE $SIZE"
+      done
+    # ask for bike to rent
 
-  # send to main menu
-  
+    # if input is not a number
+
+    # send to main menu
+
+  fi
 }
 
 RETURN_MENU() {
