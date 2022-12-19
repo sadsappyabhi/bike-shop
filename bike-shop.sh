84178fe0d61e9db6a3745eee3dba687ade8745cc
abhi <ajayanthi2@gmail.com>
2022-12-19 13:47:31 -0500

    first commit

---
+++ bike-shop.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#Create a PSQL Variable so you can query psql with $($PSQL "<query here>")
+PSQL="psql -X --username=freecodecamp --dbname=bikes --tuples-only -c"
+echo -e "\n~~~~~ Bike Rental Shop ~~~~~\n"
+
+
+#Build Main menu first, create dummy sub menus with echo statements to test flow
+MAIN_MENU() {
+#Checks if MAIN_MENU function call is followed by an argument - if it is, print the argument
+  if [[ $1 ]]
+  then
+    echo -e "\n$1"
+  fi
+#Begin menu here
+  echo "How may I help you?" 
+  echo -e "\n1. Rent a bike\n2. Return a bike\n3. Exit"
+  read MAIN_MENU_SELECTION
+
+  case $MAIN_MENU_SELECTION in
+    1) RENT_MENU ;;
+    2) RETURN_MENU ;;
+    3) EXIT ;;
+    *) MAIN_MENU "Please enter a valid option." ;;
+  esac
+}
+
+RENT_MENU() {
+  # get available bikes
+
+  # if no bikes available
+
+  # send to main menu
+  
+}
+
+RETURN_MENU() {
+  echo "Return Menu"
+}
+
+EXIT() {
+  echo -e "\nThank you for stopping in.\n"
+}
+
+MAIN_MENU
+
